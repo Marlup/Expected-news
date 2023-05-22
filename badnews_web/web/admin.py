930fba940d767f7b7515fae36269de8703ff06b4
Marlo <51297824+Marlup@users.noreply.github.com>
2023-05-22 10:42:27 +0200

    Delete badnews_web directory

--- badnews_web/web/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Noticia
-# Register your models here.
-admin.site.register(Noticia)
--- badnews_web/web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import web_lista_noticias
-#
-urlpatterns = [
-    path('', web_lista_noticias, name='web_lista_noticias'),
-    path('page=<int:page>/', web_lista_noticias, name='web_lista_noticias'),
-]
-
