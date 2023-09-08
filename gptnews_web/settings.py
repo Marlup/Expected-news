a2d28746f0191fbe011529ee1278d36e1482f023
Marlo <marsunlop@gmail.com>
2023-09-08 21:54:48 +0200

    a

--- gptnews_web/settings.py
+++ gptnews_web/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'gptnews_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'fast_news.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
