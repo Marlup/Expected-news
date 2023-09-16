11be0f385c522ec5a21c9404c67b75d44636711a
Marlo <marsunlop@gmail.com>
2023-09-16 23:51:50 +0200

    infinite-scroll-by-topic added; margins added

--- news/server_variables.py
+++ news/server_variables.py
@@ -1,2 +1,7 @@
-# Constant values
+## Numbers and bools
 N_SHOWN_FEED_ROWS = 10
+N_ELEMENTS_LOAD = 10
+## Paths ##
+# Template
+PATH_HTML_FEED = './web/feed.html'
+#PATH_HTML_FEED_BY_TOPIC = './web/feed-by-topic.html'
--- news/views.py
+++ news/views.py
@@ -1,88 +1,75 @@
 from django.shortcuts import render#, get_object_or_404
 from .models import News
 from .server_variables import *
+from django.http import JsonResponse
 #from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-PATH_HTML_MAIN_FEED = './web/main-feed.html'
-PATH_HTML_FEED_TOPIC = './web/feed-by-topic.html'
+# Auxiliar queries
+def _base_query():
+     return News.objects.values("url",
+                                "creationDate",
+                                "insertDate", 
+                                "title",
+                                "description", 
+                                "articleBody",
+                                "imageUrl",
+                                "score",
+                                "preprocessed"
+                                )
+def _end_query(query, 
+               offset=0,
+               n_rows=N_SHOWN_FEED_ROWS
+               ):
+     # "-column", means "order by column descending order"
+     rows = query.filter(preprocessed=True) \
+                 .order_by("-creationDate", 
+                           "-score") \
+                 .all()[offset:offset + n_rows]
+     return rows
 
-# Create your views here.
-def view_main_feed(request, 
-                   n_rows=N_SHOWN_FEED_ROWS):
-    query_set = News.objects.values("url",
-                                    "creationDate",
-                                    "insertDate", 
-                                    "title",
-                                    "description", 
-                                    "articleBody",
-                                    "imageUrl",
-                                    "score",
-                                    "preprocessed"
-                                    )
-    return _view_main_feed(request, 
-                           PATH_HTML_MAIN_FEED,
-                           query_set, 
-                           n_rows)
-
-def view_main_feed_by_topic(request, 
-                            topic,
-                            n_rows=N_SHOWN_FEED_ROWS):
-    query_set = News.objects.values("url",
-                                    "creationDate",
-                                    "insertDate", 
-                                    "title",
-                                    "description", 
-                                    "articleBody", 
-                                    "mainTopic",
-                                    "imageUrl",
-                                    "score",
-                                    "preprocessed"
-                                    ) \
-                            .filter(mainTopic__icontains=topic)
-    return _view_main_feed(request, 
-                           PATH_HTML_FEED_TOPIC,
-                           query_set, 
-                           n_rows)
-
-def _view_main_feed(request,
-                    path,
-                    pre_query, 
-                    n_rows):
-        # "-column", means "order by column descending order"
-        rows = pre_query.filter(preprocessed=True) \
-                        .order_by("-creationDate", 
-                                  "-score") \
-                        .all()[:n_rows]
-        data_context = {
-             "rows": rows
-             }
+# filter() note: https://chat.openai.com/c/946fa933-8cd3-4327-bb93-95db6a31fc13
+def _view_feed(request,
+               path,
+               query):
+        data_context = {"rows": _end_query(query)}
         return render(request, 
                       path, 
                       data_context)
 
+## Views ##
+# Main feed view
+def view_feed(request):
+    return _view_feed(request, 
+                      PATH_HTML_FEED,
+                      _base_query())
 
-def view_main_feed_by_topic2(request, 
-                            topic,
-                            n_rows=N_SHOWN_FEED_ROWS):
-    rows = News.objects.filter(preprocessed=True, 
-                               mainTopic__icontains=topic) \
-                       .values("url",
-                               "creationDate",
-                               "insertDate", 
-                               "title",
-                               "description", 
-                               "articleBody", 
-                               "mainTopic",
-                               "imageUrl",
-                               "score",
-                               "preprocessed"
-                               ) \
-                       .order_by("-creationDate", 
-                                 "-score") \
-                       .all()[:n_rows]
-    data_context = {
-          "rows": rows
-          }
-    return render(request, 
-                    PATH_HTML_FEED_TOPIC, 
-                    data_context)
+# Feed view by topic selected by user
+def view_feed_by_topic(request, 
+                       topic):
+    return _view_feed(request, 
+                      PATH_HTML_FEED,
+                      _base_query().filter(mainTopic__icontains=topic))
+
+## Data loading ##
+# Get more data when the bottom of page is reached
+def get_more_news(request,
+                  offset
+                  ):
+        #offset = int(request.GET.get("offset", 0))
+    query = _base_query()
+    rows = _end_query(query, 
+                      offset)
+    print(rows, offset)
+    return JsonResponse({"rows": list(rows)})
+def get_more_news_by_topic(request,
+                           topic,
+                           offset
+                           ):
+    #offset = int(request.GET.get("offset", 0))
+    query = _base_query().filter(mainTopic__icontains=topic)
+    rows = _end_query(query, 
+                      offset)
+    #print(type(rows), type(list(rows)), offset)
+    return JsonResponse({"rows": list(rows)}, 
+                        content_type="application/json"
+                        )
